a148540ec19e4d8cb138985b1723280b73767f5b
Pedro Santos <pedrosantosvz@outlook.com>
2025-09-05 14:07:56 -0300

    fix(community): layout

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -11,11 +11,11 @@ export function Card({ title, author, description, downloads }: Props) {
   return (
     <div className="m-2">
       <div className="hover:bg-accent bg flex flex-col rounded border p-4 hover:cursor-pointer">
-        <span className="p-2">{title}</span>
+        <span className="p-2 text-lg font-semibold">{title}</span>
         <span className="p-2">{author}</span>
         <span>{description}</span>
       </div>
-      <div className="flex pt-2">
+      <div className="flex pt-2 pl-2">
         <Download size={16} />
         <span className="pl-2 text-sm">{downloads}</span>
       </div>
--- src/services/CommunityService.tsx
+++ src/services/CommunityService.tsx
@@ -1,17 +1,10 @@
 import { apiCall } from '@/api/api-client'
-import axios from 'axios'
-
-const url = 'http://localhost:3000'
 
 const CommunityService = {
   getTemplates: async () => {
     const res = await apiCall('GET', '/community')
 
     return res.json()
-  },
-
-  deleteTemplate: async (id: string) => {
-    return axios.delete(`${url}/templates/${id}`)
   },
 }
 
